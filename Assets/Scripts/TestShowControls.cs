52be28c587dd3d7f45600ce631baf0f68c689dc4
Jason Viers <bean@beanalby.net>
2012-09-09 03:31:00 -0400

    Removed unneeded files from TwinStickShooter

---
+++ Assets/Scripts/TestShowControls.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestShowControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ShowControls.CreateDock(new ArrayList(new[] {
+                new ControlItem("Test", KeyCode.E)}));
+        }
+	}
+}
