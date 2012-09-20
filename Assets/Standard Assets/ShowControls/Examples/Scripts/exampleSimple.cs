21197a39eb1b49d77261286fa450378eacbaefc4
Jason Viers <bean@beanalby.net>
2012-09-20 01:15:35 -0400

    Fixed typo in example

--- Assets/Standard Assets/ShowControls/Examples/Scripts/exampleSimple.cs
+++ Assets/Standard Assets/ShowControls/Examples/Scripts/exampleSimple.cs
@@ -39,7 +39,7 @@ public class exampleSimple : MonoBehaviour
         style.wordWrap = true;
         style.normal.textColor = Color.white;
         GUI.Label(new Rect(Screen.width / 4, Screen.height / 4, Screen.width / 2, Screen.height / 2),
-            "Press a, s, d, or f.  They'll overwrap if you "
+            "Press a, s, d, or f.  They'll overlap if you "
             + "mash keys, but presumably you'll be introducing new keys rarely enough "
             + "that they won't be immediately after each other.\n\n"
             + "Note that we call .Show() immediately on the returned ShowControl objects.\n\n",
